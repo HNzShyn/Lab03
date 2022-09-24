2cd413b36ec66b2f38ee97f42b74e4112513649c
HNzShyn <nvu1343@gmail.com>
2022-09-24 16:01:40 +0700

    ok

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const http = require("http");
-http
-  .createServer((req, res) => {
+const fs = require("fs");
+http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
+    if(req.url == "/"){
+        fs.readFile("./index.js", (err, data) =>{
+            if(err){
+                console.log(err);
+            }else{
+                res.write(data);
+                res.end();
+            }
+        })
+    }else if(req.url == "/greenwich"){
+        fs.readFile("./greenwich.html", (err, data) => {
+            if(err){
+                console.log(err);
+            }else{
+                res.write(data);
+                res.end();
+            }
+        })
+    }else if(req.url == "/cloud.html"){
+        fs.readFile("./cloud.html", (err, data) => {
+            if(err){
+                console.log(err);
+            }else{
+                res.write(data);
+                res.end();
+            }
+        })
+    }
     res.write("Welcome!!");
     res.end("<h1>Heroku deployment tutorial</h1>");
   })
